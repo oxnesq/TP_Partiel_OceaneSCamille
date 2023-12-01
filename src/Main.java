8bd1e225b8a90538e184bfb129d48d3e5e67cc8e
CamilleKhv <camille.kerherve@gmail.com>
2023-12-01 10:05:27 +0100

    exception

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
-import java.sql.SQLOutput;
+
 import java.util.ArrayList;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws HopitalException{
         System.out.printf("Hopital!"+'\n');
 
         Service serviceUrologie= new Service("Urologie",1,5,5,"Ambulatoire");
